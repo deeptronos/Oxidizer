ac422212c19712da7dab140e0e40351302c623f1
Catherine <c.n@pdx.edu>
2024-12-03 13:38:35 -0800

    Added arg parsing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 
 parser = argparse.ArgumentParser()
+parser.add_argument("-v", help="Verbose output (Debug)", action="store_true")
 parser.add_argument("file", help="The audio file (WAV) to be processed.", type=str)
 parser.parse_args()
